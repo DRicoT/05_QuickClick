17800fdafbca4d45a80fce1bbc931d1da27dad66
DRicoT <wasnaisxp@gmail.com>
2022-01-05 20:03:23 +0100

    Ejercicio_108

--- Assets/_Scripts/Target.cs
+++ Assets/_Scripts/Target.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+
+    [SerializeField] private float minForce = 12;
+    [SerializeField] private float maxForce = 17;
+    [SerializeField] private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -5;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(),RandomTorque(), RandomTorque(),ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
@@ -17,4 +29,46 @@ public class Target : MonoBehaviour
     {
         
     }
+    
+    /// <summary>
+    /// Genera un vector aleatorio en 3D
+    /// </summary>
+    /// <returns>Fuerza aleatoria hacia arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+    
+    /// <summary>
+    /// Genera un número aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque y maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    
+    /// <summary>
+    /// Genera una posición aleatoria
+    /// </summary>
+    /// <returns>Posición aleatoria en 3D, con la coordenada z = 0</returns>
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos); // z = 0
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("KillZone"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    
 }
